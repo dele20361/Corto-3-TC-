46daabd5d7f664ee602e4a7fd4b59afefe234ab9
Juan Angel Carrera <angelitocarrera@gmail.com>
2022-11-10 23:30:45 -0600

    Ejercicio 2 y 4

--- corto3.py
+++ corto3.py
@@ -15,3 +15,25 @@ D = [
     ]
 
 print (sorted(D, key = lambda i:i['make']))
+
+
+#Ejercicio No.2, Potencia de una lista
+
+listpower = lambda x,y: [i**y for i in x]
+
+print(listpower([2,3,5,7,11,13],2))
+
+print(listpower([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],3))
+
+#Ejercicio No.3, Calcular la Transpuesta de una Matriz
+
+#Ejercicio No.4, Reverso de una lista de strings
+
+listreverse = lambda x: [i[::-1] for i in x]
+
+print(listreverse(['abc','def','ghi']))
+print(listreverse(['rojo', 'verde', 'aZul', 'Blanco', 'negro'],))
+
+#Ejercicio No.5, Eliminar elementos de una lista dada
+
+
