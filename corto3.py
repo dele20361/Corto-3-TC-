cdfea1bf0fa0be2df14c22cd393e43863a13de27
Paola De León <64100118+dele20361@users.noreply.github.com>
2022-11-11 20:01:06 -0600

    Ejercicio 5

--- corto3.py
+++ corto3.py
@@ -6,7 +6,11 @@
 # Cristian Aguirre 20231
 
 
-#Ejercicio No.1, ordernar por medio de una key la lista de diccionarios
+print('\n\nCORTO NO.3')
+
+#---------------------------------------------------------------------------#
+#  Ejercicio No.1, ordernar por medio de una key la lista de diccionarios
+#---------------------------------------------------------------------------#
 D = [
     {"make": "Nokia",   "model": 216, "color": "Black"},
     {"make": "Apple",   "model": 2,   "color": "Silver"}, 
@@ -14,26 +18,40 @@ D = [
     {"make": "Samsung", "model": 7,   "color": "Blue"}
     ]
 
+print('\n- Ejercicio 1')
 print (sorted(D, key = lambda i:i['make']))
 
-
-#Ejercicio No.2, Potencia de una lista
+#---------------------------------------------------------------------------#
+#                  Ejercicio No.2, Potencia de una lista
+#---------------------------------------------------------------------------#
 
 listpower = lambda x,y: [i**y for i in x]
 
+print('\n\n- Ejercicio 2')
 print(listpower([2,3,5,7,11,13],2))
-
 print(listpower([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],3))
 
-#Ejercicio No.3, Calcular la Transpuesta de una Matriz
+#---------------------------------------------------------------------------#
+#        Ejercicio No.3, Calcular la Transpuesta de una Matriz
+#---------------------------------------------------------------------------#
+print('\n\n- Ejercicio 3')
 
-#Ejercicio No.4, Reverso de una lista de strings
+#---------------------------------------------------------------------------#
+#               Ejercicio No.4, Reverso de una lista de strings
+#---------------------------------------------------------------------------#
 
 listreverse = lambda x: [i[::-1] for i in x]
 
+print('\n\n- Ejercicio 4')
 print(listreverse(['abc','def','ghi']))
 print(listreverse(['rojo', 'verde', 'aZul', 'Blanco', 'negro'],))
 
-#Ejercicio No.5, Eliminar elementos de una lista dada
 
+#---------------------------------------------------------------------------#
+#          Ejercicio No.5, Eliminar elementos de una lista dada
+#---------------------------------------------------------------------------#
+elem = ['rojo', 'verde', 'azul', 'amarillo', 'gris', 'blanco', 'negro']
+deteleElem = lambda x:  [i for i in elem if i not in x]
+print('\n\n- Ejercicio 5')
+print(deteleElem(['amarillo', 'gris']))
 
